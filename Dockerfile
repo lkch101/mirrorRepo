abdd53852793a5438d870898b76ea5d50267a6c2
lkch101 <57426398+lkch101@users.noreply.github.com>
2023-03-11 22:08:22 +0800

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,39 @@
 FROM centos:7.9.2009
 WORKDIR /etc/yum.repos.d
 ADD CentOS-Base.repo .
+
 RUN cat /etc/yum.repos.d/CentOS-Base.repo && /usr/bin/yum clean all && /usr/bin/yum makecache
 RUN /usr/bin/rpm -Uvh http://nginx.org/packages/centos/7/noarch/RPMS/nginx-release-centos-7-0.el7.ngx.noarch.rpm
 RUN /usr/bin/yum install -y wget yum-utils createrepo  make cmake gcc gcc-c++ pcre-devel zlib-devel \
     openssl openssl-devel net-tools vim nginx yum-plugin-downloadonly rsync && \
-	/usr/bin/yum clean all 
-RUN /usr/bin/mkdir -p /usr/share/nginx/html/mirrors/centos && /usr/bin/createrepo /usr/share/nginx/html/mirrors/centos
-WORKDIR /etc/nginx/conf.d
+	/usr/bin/yum clean all
+
+ENV mirror_nignx_conf_dir	/etc/nginx/conf.d
+ENV mirror_nignx_data_dir	/usr/share/nginx/html/mirrors
+ENV mirror_centosplus 		$mirror_nignx_data_dir/centos/7/centosplus/x86_64/
+ENV mirror_extras 			$mirror_nignx_data_dir/centos/7/extras/x86_64/
+ENV mirror_os 				$mirror_nignx_data_dir/centos/7/os/x86_64/
+ENV mirror_updates 			$mirror_nignx_data_dir/centos/7/updates/x86_64/
+ENV mirror_kubernetes 		$mirror_nignx_data_dir/centos/7/kubernetes/x86_64/
+
+RUN mkdir -p $mirror_centosplus/Packages/ && \
+	mkdir -p $mirror_extras/Packages/ && \ 	
+	mkdir -p $mirror_os/Packages/ && \ 		
+	mkdir -p $mirror_updates/Packages/ && \ 	
+	mkdir -p $mirror_kubernetes/Packages/ && \
+	/usr/bin/createrepo  $mirror_centosplus && \
+	/usr/bin/createrepo  $mirror_extras && \
+	/usr/bin/createrepo  $mirror_os && \
+	/usr/bin/createrepo  $mirror_updates && \ 	
+	/usr/bin/createrepo  $mirror_kubernetes
+
+WORKDIR $mirror_nignx_conf_dir
 ADD default.conf .
-WORKDIR /usr/share/nginx/html/mirrors
+
+WORKDIR $mirror_nignx_data_dir
 ADD mirrors.repo .
 ADD update_repo_yum_tsinghua.sh .
+
 EXPOSE 80
 CMD echo "port 80 workdir /var/www/html/mirrors"
 CMD ["nginx","-g","daemon off;"]
--- update_repo_yum_tsinghua.sh
+++ update_repo_yum_tsinghua.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-/usr/bin/rsync -avz rsync://mirrors.tuna.tsinghua.edu.cn/centos/7/centosplus/x86_64/Packages/ /usr/share/nginx/html/mirrors/centos
-/usr/bin/rsync -avz rsync://mirrors.tuna.tsinghua.edu.cn/centos/7/extras/x86_64/Packages/ /usr/share/nginx/html/mirrors/centos
-/usr/bin/rsync -avz rsync://mirrors.tuna.tsinghua.edu.cn/centos/7/os/x86_64/Packages/ /usr/share/nginx/html/mirrors/centos
-/usr/bin/rsync -avz rsync://mirrors.tuna.tsinghua.edu.cn/centos/7/updates/x86_64/Packages/ /usr/share/nginx/html/mirrors/centos
-/usr/bin/rsync -avz rsync://mirrors.tuna.tsinghua.edu.cn/epel/7Server/x86_64/Packages/ /usr/share/nginx/html/mirrors/centos
-/usr/bin/rsync -avz rsync://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-x86_64/Packages/ /usr/share/nginx/html/mirrors/centos
-#/usr/bin/rsync -avz rsync://download.docker.com/linux/centos/7/x86_64/stable/Packages/ /var/www/html/mirrors/centos
+update_mirror_nignx_data_dir=/usr/share/nginx/html/mirrors
+update_mirror_centosplus=$mirror_nignx_data_dir/centos/7/centosplus/x86_64/Packages/
+update_mirror_extras=$mirror_nignx_data_dir/centos/7/extras/x86_64/Packages/
+update_mirror_os=$mirror_nignx_data_dir/centos/7/os/x86_64/Packages/
+update_mirror_updates=$mirror_nignx_data_dir/centos/7/updates/x86_64/Packages/
+update_mirror_kubernetes=$mirror_nignx_data_dir/centos/7/kubernetes/x86_64/Packages/
 
-/usr/bin/createrepo --update /var/www/html/mirrors/centos
+/usr/bin/rsync -avz rsync://mirrors.tuna.tsinghua.edu.cn/centos/7/centosplus/x86_64/Packages/ 			$update_mirror_centosplus
+/usr/bin/rsync -avz rsync://mirrors.tuna.tsinghua.edu.cn/centos/7/extras/x86_64/Packages/ 				$update_mirror_extras
+/usr/bin/rsync -avz rsync://mirrors.tuna.tsinghua.edu.cn/centos/7/os/x86_64/Packages/ 					$update_mirror_os
+/usr/bin/rsync -avz rsync://mirrors.tuna.tsinghua.edu.cn/centos/7/updates/x86_64/Packages/ 				$update_mirror_updates
+/usr/bin/rsync -avz rsync://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-x86_64/Packages/ 	$update_mirror_kubernetes
+
+/usr/bin/createrepo --update $update_mirror_centosplus
+/usr/bin/createrepo --update $update_mirror_extras
+/usr/bin/createrepo --update $update_mirror_os
+/usr/bin/createrepo --update $update_mirror_updates
+/usr/bin/createrepo --update $update_mirror_kubernetes
